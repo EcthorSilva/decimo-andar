e5f380bfda5cb4b3e6d8e9c7816530bce4ec9e36
Ecthor Silva <EcthorSilva@outlook.com>
2024-04-20 21:53:39 -0300

    Tela de cadastro (#3)
    * Add: Iniciando o processo de criação do cadastro de usuario
    
    * Criado a primeira parte da logica de criação do usuario/cliente do nosso site
    
    * Correção na função que altera o cpf para cnpj na tela da cadastro

---
+++ DecimoAndar/src/main/java/br/com/decimoandar/servlet/CreateUserServlet.java
@@ -0,0 +1,37 @@
+package br.com.decimoandar.servlet;
+
+import br.com.decimoandar.dao.UserDao;
+import br.com.decimoandar.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+@WebServlet("/create-user")
+public class CreateUserServlet extends HttpServlet {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = request.getReader();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+
+        String jsonData = stringBuilder.toString();
+
+        User user = objectMapper.readValue(jsonData, User.class);
+
+        UserDao userDao = new UserDao();
+        userDao.createUser(user);
+
+        response.sendRedirect("/index.html");
+    }
+}
