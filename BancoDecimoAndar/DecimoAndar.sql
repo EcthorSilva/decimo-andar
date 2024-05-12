fc05f775ba0efd8e43e7a3cf3ec6c33440043c03
Ecthor Silva <EcthorSilva@outlook.com>
2024-05-12 20:00:10 -0300

    Change: modificação no script sql

--- BancoDecimoAndar/DecimoAndar.sql
+++ BancoDecimoAndar/DecimoAndar.sql
@@ -3,14 +3,20 @@ create schema DECIMO_ANDAR;
 use DECIMO_ANDAR;
 
 CREATE TABLE DECIMO_ANDAR.Imovel (
-    id BIGINT PRIMARY KEY auto_increment,
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
     tipo VARCHAR(255),
-    quantidadeQuartos INTEGER,
-    metrosQuadrados DOUBLE ,
-    cep VARCHAR(10) ,
-    logradouro VARCHAR(255) ,
+    tipoVenda VARCHAR(255),
+    valor DECIMAL(10, 2),
+    endereco VARCHAR(255),
+    numero VARCHAR(20),
+    cidade VARCHAR(255),
+    uf VARCHAR(2),
+    cep VARCHAR(10),
+    numQuartos VARCHAR(255),
+    numBanheiros VARCHAR(255),
+    metrosQuadrados DECIMAL(10, 2),
     descricao TEXT,
-    quantidadeBanheiros INTEGER
+    USER_ID INTEGER
 );
 
 CREATE TABLE DECIMO_ANDAR.Usuario (
