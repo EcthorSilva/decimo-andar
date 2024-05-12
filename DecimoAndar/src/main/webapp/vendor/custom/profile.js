8773224b0ef0fab2e9dc467a057d877c00dbf993
Ecthor Silva <EcthorSilva@outlook.com>
2024-05-11 21:46:24 -0300

    add: terminei a função apara apresentar os dados do usuario no perfil

--- DecimoAndar/src/main/webapp/vendor/custom/profile.js
+++ DecimoAndar/src/main/webapp/vendor/custom/profile.js
@@ -1,9 +1,38 @@
-console.log("profile.js carregado!");
+console.log("profile.js carregado! \n");
 
 document.addEventListener("DOMContentLoaded", function () {
-function mostrarDadosUsuario() {
+    // Função para obter os dados do usuário
+    function obterDadosUsuario() {
         var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    var userData = JSON.parse(xhr.responseText);
+                    atualizarPerfil(userData);
+                } else {
+                    console.error('Erro ao obter os dados do usuário.');
+                }
+            }
+        };
+        xhr.open('GET', '/profile', true);
+        xhr.send();
+    }
+
+    // Função para atualizar o perfil na página
+    function atualizarPerfil(userData) {
+        document.getElementById('nome').textContent = userData.name;
+        document.getElementById('nome2').textContent = userData.name;
+        document.getElementById('email').textContent = userData.email;
+        // Adicione outras linhas de código semelhantes para atualizar outros campos, se necessário
+    }
+
+    // Chamada da função para obter os dados do usuário quando a página é carregada
+    obterDadosUsuario();
+
+
+    function mostrarDadosUsuario() {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
                 if (xhr.status === 200) {
                     var userData = JSON.parse(xhr.responseText);
