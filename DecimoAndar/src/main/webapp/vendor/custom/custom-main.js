03c2a3cd3efa49d1a6fabd4cf6dcadb5ece7a879
Guilherme Soares <126208556+Guisoaresz13@users.noreply.github.com>
2024-05-14 13:34:00 -0300

    Adicionado: Diretório aos botões Anunciar em Header (#20)

--- DecimoAndar/src/main/webapp/vendor/custom/custom-main.js
+++ DecimoAndar/src/main/webapp/vendor/custom/custom-main.js
@@ -50,6 +50,16 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
+    // Direcionando Click do btn "Anunciar" na Header.
+    document.getElementById("AnunciarHeader").addEventListener("click", function() {
+            console.log("Passei aqui zé da bronha")
+        if (checkSessionCookie()) {
+            window.location.href = "/pages/imovel.html"
+        } else {
+            window.location.href = "/pages/login.html"
+        }
+    });
+
     updateButtonVisibility();
 
     // Adicionando a função de logout ao clique do botão de logout
